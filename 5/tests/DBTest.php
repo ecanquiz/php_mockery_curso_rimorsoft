6b218c574a82355f311a4fa71b0db13ef7b29e84
Ernesto Canquiz <fdsoil123@gmail.com>
2020-06-07 10:07:53 -0400

    Test of own class with Mockery

---
+++ 5/tests/DBTest.php
@@ -0,0 +1,26 @@
+<?php
+
+use \PHPUnit\Framework\TestCase;
+use \App\DB;
+
+class DBTest extends TestCase
+{
+
+    public function test_with_mockery()
+    {
+        $db = Mockery::mock('DB');
+        $db->shouldReceive('get')->andReturn(true);
+
+        $this->assertTrue($db->get('users'));
+    }
+
+    public function test_withot_mockery()
+    {
+        $db = new DB;
+
+        $this->assertTrue($db->get('users'));
+    }
+    
+}
+
+
